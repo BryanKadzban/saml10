01e2a9ab64f011044ba38f7940df54b6bcb43720
Bryan Kadzban <github@kadzban.net>
2020-10-07 11:51:19 -0700

    Add a range to the clock divisor field in GENCTRLs

--- saml10e15a/src/gclk/genctrl.rs
+++ saml10e15a/src/gclk/genctrl.rs
@@ -309,7 +309,7 @@ pub struct DIV_W<'a> {
 impl<'a> DIV_W<'a> {
     #[doc = r"Writes raw bits to the field"]
     #[inline(always)]
-    pub unsafe fn bits(self, value: u16) -> &'a mut W {
+    pub fn bits(self, value: u16) -> &'a mut W {
         self.w.bits = (self.w.bits & !(0xffff << 16)) | (((value as u32) & 0xffff) << 16);
         self.w
     }
